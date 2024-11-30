43e691eaffd050e4dc0638caea461b9010b9cfb1
Hussein Elhawary <husseinelhawary@gmail.com>
2024-11-30 21:29:34 +0200

    structure initialization

---
+++ src/feature_extraction.py
@@ -0,0 +1,13 @@
+def extract_tfidf_features(data):
+    """Extract TF-IDF features."""
+    pass
+
+def extract_embeddings_word2vec(data):
+    """Extract embeddings Word2Vec"""
+    pass
+
+def contextual_embeddings(data):
+    """Extract embeddings contextual embeddings"""
+
+def hybrid_features(data):
+    """Extract hybrid features by combining two different features"""
---
+++ src/preprocess.py
@@ -0,0 +1,12 @@
+
+def clean_text(text):
+    """Remove unwanted characters and symbols from text."""
+    pass
+
+def tokenize_text(text, tokenizer):
+    """Tokenize text using a given tokenizer."""
+    pass
+
+def lemmatize_text(tokens, lemmatizer):
+    """Lemmatize tokens."""
+    pass
